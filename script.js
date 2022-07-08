629e2776454875bbae0fbe2e62cb967de780a243
slumpthug <gangguchi58@gmail.com>
2022-07-08 22:31:58 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+//Задание 1
+// const arr = [{ id: 1, name: 'Piter'}, { id: 2, name: 'Nina' }];
+
+// console.log(arr.find(item => item.id == 2));
+
+//Задание 2
+// const arr = [{num: 1, floor: 1, people: 2,}, {num: 2, floor: 1, people: 4,}, {num: 3, floor: 2, people: 1,}, {num: 4, floor: 2, people: 3,}];
+
+// const result = arr.filter(item => item.people < 3);
+
+// console.log(result);
+
+// const resultNum = [result[0].num, result[1].num]
+
+// console.log(resultNum);
+
+//Задание 3
+// const arr = [1, 2, 3, 4, 5];
+
+// arr.splice(3, 0, 'a', 'b', 'c');
+
+// console.log(arr);
+
+//Задание 4
+// const arr = [1, 2, 3, 4, 5];
+
+// const arr2 = arr.slice(3, 5);
+
+// console.log(arr2);
+
+//Задание 5
+// let mas = [15, 'hello', 'trump', 23, 'world', 999, 176];
+
+// const masStr = mas.filter(item => typeof item === 'string');
+
+// console.log(masStr);
+
+// console.log(masStr.join('---'));
